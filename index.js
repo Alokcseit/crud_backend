5a3ff3fa6b661c41e9fc009a2b2e13b0efc2381b
Alokcseit <swainalokkumar25@gmail.com>
2025-12-13 16:48:15 +0530

    second commit

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ require("dotenv").config({quiet:true})
 const { app } = require("./app")
 const {db} = require("./db/db")
 const cors =require("cors")
-app.use(cors())
+app.use(cors(
+  {
+    origin:"*",
+    methods:['GET', 'POST', 'PUT', 'DELETE']
+  }
+))
 db()
 
 app.listen(process.env.PORT, () => {
