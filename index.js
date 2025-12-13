3a8ea931915ccb904d1624ec80c9a4e9e28584af
Alokcseit <swainalokkumar25@gmail.com>
2025-12-13 17:12:49 +0530

    fourth commit

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const {db} = require("./db/db")
 const cors =require("cors")
 app.use(cors(
   {
-    origin:"http://localhost:5173/",
-    methods:['GET', 'POST', 'PUT', 'DELETE']
+    origin:"http://localhost:5173",
+    methods:['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
   }
 ))
 db()
